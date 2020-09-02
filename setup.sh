16743ee22ceb9e093066b03f25f0372e73048b8c
Labhairi Mouaad <thehell@M.L>
2020-09-02 17:40:50 -0400

    some changes

--- setup.sh
+++ setup.sh
@@ -2,18 +2,19 @@
 
 
 echo "Minikube starting..."
-minikube start
-
+minikube --vm-driver=docker start 
 
 eval $(minikube docker-env)
+
+minikube dashboard & 
 
 # Building Dockerfile
 echo "Building Dockerfiles"
 docker build -t nginx_service ./srcs/nginx
-docker build -t ftps_service  ./srcs/ftps
+docker build -t ftps_service  ./srcs/ftps 
 
 # Installing Metallb
-echo "Insralling Metallb"
+echo "Installing Metallb"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 # On first install only
