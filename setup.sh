379130af30ffca3c048b95396e42e080b93a159a
Labhairi Mouaad <thehell@M.L>
2020-09-05 18:58:40 -0400

    this ftps ...

--- setup.sh
+++ setup.sh
@@ -4,13 +4,16 @@
 echo "Minikube starting..."
 minikube --vm-driver=docker start 
 
+
 eval $(minikube docker-env)
+
+export MINIKUBE_IP=$(minikube ip)
 
 minikube dashboard & 
 
 # Building Dockerfile
 echo "Building Dockerfiles"
-docker build -t nginx_service ./srcs/nginx
+#docker build -t nginx_service ./srcs/nginx
 docker build -t ftps_service  ./srcs/ftps 
 
 # Installing Metallb
@@ -23,4 +26,5 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 
 # Creating Pods and Services
 echo "Creating Services and Pods"
-kubectl create -f ./srcs
+kubectl create -f ./srcs/configMap.yaml
+kubectl create -f ./srcs/ftps.yaml
