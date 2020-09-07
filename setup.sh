a9196f984beb6618a6294113a211ea1d45ba77bb
Labhairi Mouaad <thehell@M.L>
2020-09-07 10:08:17 -0400

    ftps fixed

--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 #!	bin/bash
 
-
 echo "Minikube starting..."
 minikube --vm-driver=docker start 
 
-
 eval $(minikube docker-env)
 
-export MINIKUBE_IP=$(minikube ip)
 
 minikube dashboard & 
 
 # Building Dockerfile
 echo "Building Dockerfiles"
-#docker build -t nginx_service ./srcs/nginx
+docker build -t nginx_service ./srcs/nginx
 docker build -t ftps_service  ./srcs/ftps 
 
 # Installing Metallb
@@ -23,8 +20,8 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-
 # Creating Pods and Services
 echo "Creating Services and Pods"
 kubectl create -f ./srcs/configMap.yaml
 kubectl create -f ./srcs/ftps.yaml
+kubectl create -f ./srcs/nginx.yaml
